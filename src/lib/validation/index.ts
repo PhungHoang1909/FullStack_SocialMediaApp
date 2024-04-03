ce8329a5cda7818bccf5556aab9f876c82a46dfc
phung hoang <lnphunghoang190903@gmail.com>
2024-04-03 12:59:15 +0700

    Create Sign-in page

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -7,3 +7,9 @@ export const SignupValidation = z.object({
   email: z.string().email(),
   password: z.string().min(8, {message: 'Must be at least 8 characters.'})
 })
+
+export const SigninValidation = z.object({
+
+  email: z.string().email(),
+  password: z.string().min(8, {message: 'Must be at least 8 characters.'})
+})
